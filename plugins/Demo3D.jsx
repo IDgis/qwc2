927d0d625a79b7625e8a871c3f62a1b89b717199
Reijer Copier <reijer.copier@idgis.nl>
2024-10-29 11:39:50 +0100

    Add cesium viewer

--- plugins/Demo3D.jsx
+++ plugins/Demo3D.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { Viewer, Cartesian3, Math as CesiumMath, Cesium3DTileset, WebMapServiceImageryProvider, WebMapTileServiceImageryProvider, ImageryLayer, Terrain, CesiumTerrainProvider } from 'cesium/Cesium';
+import "cesium/Widgets/widgets.css";
+
 import './style/Demo3D.css';
 
 class Demo3D extends React.Component {
 
     state = {
-        active: false
+        active: false,
+        viewer: null
     };
 
     constructor(props) {
@@ -27,11 +31,60 @@ class Demo3D extends React.Component {
     }
 
     attach() {
-        console.log("attach", this.targetRef.current);
+        console.log("attach");
+
+        const viewer = new Viewer(this.targetRef.current, {
+            animation: false, // Disable the animation widget
+            timeline: false, // Disable the timeline widget
+            fullscreenButton: false, // Disable the fullscreen button
+            homeButton: false, // Disable the home button
+            infoBox: false, // Disable the info box
+            selectionIndicator: false, // Disable the selection indicator
+            navigationHelpButton: false, // Disable the navigation help button
+            sceneModePicker: false, // Disable the scene mode picker
+            baseLayerPicker: false, // Disable the base layer picker
+            geocoder: false, // Disable the geocoder
+            vrButton: false, // Disable the VR button
+    
+            baseLayer: false,
+            terrain: new Terrain(CesiumTerrainProvider.fromUrl('https://api.pdok.nl/kadaster/3d-basisvoorziening/ogc/v1_0/collections/digitaalterreinmodel/quantized-mesh'))
+        });
+    
+        viewer.scene.globe.depthTestAgainstTerrain = true;
+
+        viewer.scene.camera.setView({
+            destination: new Cartesian3.fromDegrees(
+              4.31707256,
+              52.0871042,
+              300,
+            ),
+            orientation: {
+              heading: CesiumMath.toRadians(0.0),
+              pitch: CesiumMath.toRadians(-45.0),
+              roll: 0.0,
+            },
+        });
+
+        const provider = new WebMapServiceImageryProvider({
+            url: "https://service.pdok.nl/hwh/luchtfotorgb/wms/v1_0",
+            layers: "Actueel_orthoHR",
+            parameters : {
+              format: "image/jpeg",
+              transparent: false
+            }
+        });
+  
+        const imageryLayer = new ImageryLayer(provider)
+        viewer.imageryLayers.add(imageryLayer);
+
+        this.setState({...this.state, viewer});
     }
 
     dispose() {
         console.log("dispose");
+
+        this.state.viewer?.destroy();
+        this.setState({...this.state, viewer: null})
     }
 
     render() {
